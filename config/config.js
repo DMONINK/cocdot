d34c1d891d853ae707819964ebe7fa97cf2ffb7e
MickySinghSahni <78903620+MickySinghSahni@users.noreply.github.com>
2022-08-28 22:47:35 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
     //CLASH OF CLAN API KEY
     apikey:'',
-    discordtoken: '', // DISCORD BOT TOKEN 
+    discordtoken: 'OTA5MzIxMTAzNzY2MzQzNzIw.GOkIML.ToluGfgaph8A47MJpqyoNbD5sIXLSHr1btrtmk', // DISCORD BOT TOKEN 
     clantag:'', // YOUR CLAN TAG
-    clientID:'', // BOT 
-    guildID:'', // GUILD ID | DON'T PUT ANYTHING IF YOU WANT THE SLASH COMMANDS TO BE ACTIVATED ON ALL GUILDS
-    language: '', // "francais" or "english" 
+    clientID:'980421328530202695', // BOT 
+    guildID:'980421328530202695', // GUILD ID | DON'T PUT ANYTHING IF YOU WANT THE SLASH COMMANDS TO BE ACTIVATED ON ALL GUILDS
+    language: 'english', // "francais" or "english" 
 }   
